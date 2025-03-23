29d730450ad849888ed83862335900c01b3ef649
kritserv <krit.serv@gmail.com>
2025-03-23 23:24:40 +0700

    change from django to bottle

---
+++ main.py
@@ -0,0 +1,119 @@
+from bottle import route, run, template, get, static_file, request, redirect
+from json import dump, load, JSONDecodeError
+
+save_file = 'data.json'
+
+@get("/static/<filepath:re:.*.css>")
+def css(filepath):
+    return static_file(filepath, root="static")
+
+@get("/static/<filepath:re:.*.ico>")
+def favicon(filepath):
+    return static_file(filepath, root="static")
+
+def load_save_file():
+    try:
+        with open(save_file, 'r') as f:
+            save_data = load(f)
+            return save_data['codes'], save_data['results'], save_data['errors']
+    except (FileNotFoundError, KeyError, JSONDecodeError):
+        return [], [], []
+
+def save_save_file(codes, results, errors):
+    with open(save_file, 'w') as f:
+        dump({'codes': codes, 'results': [str(result) for result in results], 'errors': errors}, f)
+
+
+@route('/add_cell')
+def add_cell():
+    codes, results, errors = load_save_file()
+    codes.append('')
+    results.append('')
+    errors.append('')
+    save_save_file(codes, results, errors)
+    return redirect('/')
+
+@route('/delete_cell/<index:int>')
+def delete_cell(index):
+    codes, results, errors = load_save_file()
+    index = int(index)
+    if 0 <= index < len(codes):
+        del codes[index]
+        del results[index]
+        del errors[index]
+        save_save_file(codes, results, errors)
+    return redirect('/')
+
+@route('/move_up/<index:int>')
+def move_up(index):
+    codes, results, errors = load_save_file()
+    index = int(index)
+
+    if 0 < index < len(codes):
+        codes[index], codes[index-1] = codes[index-1], codes[index]
+        results[index], results[index-1] = results[index-1], results[index]
+        errors[index], errors[index-1] = errors[index-1], errors[index]
+        save_save_file(codes, results, errors)
+
+    return redirect('/')
+
+@route('/move_down/<index:int>')
+def move_down(index):
+    codes, results, errors = load_save_file()
+    index = int(index)
+
+    if 0 <= index < len(codes) - 1:
+        codes[index], codes[index+1] = codes[index+1], codes[index]
+        results[index], results[index+1] = results[index+1], results[index]
+        errors[index], errors[index+1] = errors[index+1], errors[index]
+        save_save_file(codes, results, errors)
+
+    return redirect('/')
+
+@route('/')
+def index():
+    codes, results, errors = load_save_file()
+    code = request.params.get('code', '')
+    index = request.params.get('index', '')
+
+    if code and index:
+        try:
+            index = int(index)
+            result = ''
+            error = ''
+            global_list = []
+
+            for line in code.split('\n'):
+                if '=' in line:
+                    if not '+=' in line and not '-=' in line and not '*=' in line and not '/=' in line:
+                        variable = line.split('=')[0]
+                        variable = variable.strip()
+                        global_list.append(f"global {variable}")
+                if 'def' in line:
+                    variable = line.split('def')[1].split('(')[0].strip()
+                    global_list.append(f"global {variable}")
+
+            try:
+                code_with_globals = '\n'.join(global_list) + '\n' + code
+                exec(code_with_globals)
+                last_line = code.split('\n')[-1]
+                if '=' not in last_line and last_line != '' and last_line != '"""' and last_line != "'''":
+                    if 'print' in last_line:
+                        last_line = last_line.replace('print(','')[:-1]
+                    result = eval(last_line)
+            except Exception as e:
+                error = str(e)
+                if error == "invalid syntax (<string>, line 1)":
+                    error = ""
+
+            if 0 <= index < len(codes):
+                codes[index] = code
+                results[index] = result
+                errors[index] = error
+                save_save_file(codes, results, errors)
+        except (ValueError, IndexError) as e:
+            print(e)
+
+    return template('index', codes=codes, results=results, errors=errors)
+
+run(host='localhost', port=8080, reloader=True, debug=True)
---
+++ views/index.tpl
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Untitled project</title>
+        <link type="text/css" href="/static/styles.css" rel="stylesheet">
+        <link id="favicon-link" rel="shortcut icon" href="/static/img_favicon.ico">
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <div id="header-logo">
+                    <a class="colab-large-icon">
+                        <svg viewBox="0 0 24 24"><!--?lit$074587426$-->
+                            <g id="colab-logo">
+                                <path d="M4.54,9.46,2.19,7.1a6.93,6.93,0,0,0,0,9.79l2.36-2.36A3.59,3.59,0,0,1,4.54,9.46Z" style="fill:orange"></path>
+                                <path d="M2.19,7.1,4.54,9.46a3.59,3.59,0,0,1,5.08,0l1.71-2.93h0l-.1-.08h0A6.93,6.93,0,0,0,2.19,7.1Z" style="fill:gold"></path>
+                                <path d="M11.34,17.46h0L9.62,14.54a3.59,3.59,0,0,1-5.08,0L2.19,16.9a6.93,6.93,0,0,0,9,.65l.11-.09" style="fill:gold"></path>
+                                <path d="M12,7.1a6.93,6.93,0,0,0,0,9.79l2.36-2.36a3.59,3.59,0,1,1,5.08-5.08L21.81,7.1A6.93,6.93,0,0,0,12,7.1Z" style="fill:gold"></path>
+                                <path d="M21.81,7.1,19.46,9.46a3.59,3.59,0,0,1-5.08,5.08L12,16.9A6.93,6.93,0,0,0,21.81,7.1Z" style="fill:orange"></path>
+                            </g>
+                        </svg>
+                    </a>
+                </div>
+                <p class="project-name">Untitled project</p>
+            </nav>
+            <a href="/add_cell">+ Code</a>
+            <hr/>
+        </header>
+        <main>
+            <p>Codes: <b>{{ len(codes) }}</b></p>
+            <ol type="i">
+                %for index, code in enumerate(codes):
+                <li class="code-container">
+                    <div class="code-tools">
+                        <a href="/delete_cell/{{ index }}">🗑️</a>
+                        %if index > 0:
+                            <a href="/move_up/{{ index }}">⬆️</a>
+                        %end
+                        %if index < len(codes)-1:
+                            <a href="/move_down/{{ index }}">⬇️</a>
+                        %end
+                    </div>
+                    <form id="form{{ index }}">
+                        <table>
+                            <tr>
+                                <td><a href="javascript:;" onclick="document.getElementById('form{{ index }}').submit()">▶</a></td>
+                                <td style="width:90vw;"><textarea class="code" name="code">{{ code }}</textarea></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <div class="code-result">
+                                        <p>{{ results[index] }}</p>
+                                        <p class="error">{{ errors[index] }}</p>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                        <input type="hidden" name="index" value="{{ index }}">
+                    </form>
+                </li>
+                %end
+            </ol>
+        </main>
+    </body>
+</html>
